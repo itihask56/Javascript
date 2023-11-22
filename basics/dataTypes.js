433432fb2f1083f2d40416be26124c0f4d293653
Itihas Verma <103826009+itihask56@users.noreply.github.com>
2023-11-22 06:51:12 +0000

    datatypes completed

---
+++ basics/dataTypes.js
@@ -0,0 +1,18 @@
+"use strict" // treat all js code as newer version
+let id =1566;//number=>2 to the power 23
+let name="Itihas";//string => ""
+let married = false;//boolean =>true/false
+let age; //undefined
+
+let val =null//standalone value
+// console.log(typeof val);
+
+let stDetails={
+    "id":1566,
+    "name":"Itihas",
+    employeed:false,
+
+}
+console.log(typeof stDetails);
+console.log([stDetails.id,stDetails.name,stDetails.employeed]);
+
