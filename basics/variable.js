39117c93ebf3622e154e4aac9ead35015824d7e0
Itihas Verma <103826009+itihask56@users.noreply.github.com>
2023-11-22 06:50:08 +0000

    variables done

---
+++ basics/variable.js
@@ -0,0 +1,20 @@
+//you cannot change the value and assign the value to a const variable in entire document
+const accId = 1566
+// accId = 1555
+
+// let keyword enable us to change and assign the value of a var declared with a let keyword
+let accEmail = "dev.verma@gmail.com"
+accEmail = "dev.itihas@gmail.com"
+
+/*prefre not to use var keyword because issue of block scope and functional scope*/
+var accPassword = 123456
+
+
+console.log([accEmail,accId,accPassword]);
+
+accCity = "Nawada"
+console.log(accCity);
+
+let accState;
+console.log(accState);
+output//undefined
