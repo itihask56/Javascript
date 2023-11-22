7a84abf57adc88b0428c40a951728f84791ba0c0
Itihas Verma <103826009+itihask56@users.noreply.github.com>
2023-11-22 07:14:03 +0000

    typeconverstion done

---
+++ basics/conversionOperations.js
@@ -0,0 +1,25 @@
+// let num = "33";
+let num =true;
+console.log(typeof num);
+
+let valInNumber = Number(num)
+console.log( typeof valInNumber);
+
+//"33"=>33
+//"33asd"=>NaN
+//true =>1
+//false =>0
+
+// let loggedIn = 1;
+let loggedIn="itihas";
+let boolLoggedIn = Boolean(loggedIn)
+console.log(boolLoggedIn);
+
+//1 =>true
+//"" =>false
+// "itihas" =>true
+
+let someNum = 33;
+console.log(typeof someNum);
+let stringSomeNum = String(someNum)
+console.log(typeof stringSomeNum);
