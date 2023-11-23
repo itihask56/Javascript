12faa6a496e7dda31467ca07f17eb2f7e75090d7
Itihas Verma <103826009+itihask56@users.noreply.github.com>
2023-11-23 04:00:30 +0000

    string continued

--- basics/dataTypes.js
+++ basics/dataTypes.js
@@ -16,3 +16,36 @@ let stDetails={
 console.log(typeof stDetails);
 console.log([stDetails.id,stDetails.name,stDetails.employeed]);
 
+
+
+
+//=/=/=/=/=/=/=/=/=/=/=/=/=/=/==/=/=/=/=/=/=/==/=/=/==/=/=/=/=/==//=/==/=/=/=/==/=/==/==/==//==/
+
+/* Data types
+Primitive datatypes stored in (stack)
+NonPrimitive datattypes(Heaps)
+*/
+
+//stack
+let myName = "Itihas";
+let myNickName = myName;
+myNickName="itihas verma";
+console.log([myName,myNickName]);
+//output:Itihas,ithas Verma
+// only copy of data is passed to the var...and there is no change in main var
+
+
+//Heap
+let st_details={
+    id:"1566",
+    name:"Itihas",
+    age:21,
+
+}
+let st2_details=st_details
+st2_details.age=22;
+console.log([st_details.age,st2_details.age])
+//output:22,22
+
+//reference of main variable is passed to the copied variable and if there is change in copied var then this change will 
+//reflect in main varibale also
---
+++ basics/string.js
@@ -0,0 +1,21 @@
+const name = "itihas";
+let repo = 31;
+
+// console.log(`I am ${name} and i have ${repo} repository on github`)
+
+const myName = new String("Itihas Verma")
+// console.log(myName.toUpperCase());
+// console.log(myName.charAt(2));
+// console.log(myName.indexOf("V"));
+ 
+
+//slicing string
+// console.log(myName.slice(7)) //verma
+// console.log(myName.slice(0,7)) //itihas
+// console.log(myName.slice(-10,7)) //itihas
+
+//trim
+const password = new String("    password.  ")
+console.log([password,password.trim()]);
+ 
+ 
