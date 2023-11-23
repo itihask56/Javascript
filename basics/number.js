2898b52d37174c3f35248462dc5e257e4bd5fa40
Itihas Verma <103826009+itihask56@users.noreply.github.com>
2023-11-23 04:35:50 +0000

    number continued

---
+++ basics/number.js
@@ -0,0 +1,18 @@
+const num =12;
+const num2=num.toString()
+// console.log(typeof num2);//string
+// console.log(num2.toUpperCase());//12
+
+const id = new Number(1566)
+// console.log(id);
+// console.log(id.valueOf())
+
+// console.log(id.toPrecision(4))//1566
+// console.log(id.toPrecision(5))//1566
+
+const newNum = new Number(123.456)
+console.log(Math.ceil(newNum))
+console.log(Math.floor(newNum))
+console.log(Math.round(newNum))
+
+
